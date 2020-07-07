7856d376dd7dcd4795f2a06b4c71438285b8af09
stursyn <stursyn@greet-go.com>
2020-07-07 14:40:03 +0600

    Сохранение номера заказа и gitignore

--- carpet_cleaner.register/main/java/kz/nu/carpet_cleaner/register/dao/OrderDao.java
+++ carpet_cleaner.register/main/java/kz/nu/carpet_cleaner/register/dao/OrderDao.java
@@ -13,8 +13,8 @@ public interface OrderDao {
   @Select("SELECT nextval('s_order_number')")
   long loadSeqOrderNumber();
 
-  @Insert("insert into clean_order(id, customerId, pickUpAddress, deliveryAddress, pickUpDate, deliveryDate, cleanStatus, totalOrderPrice) " +
-      " values (#{toSave.id}, #{toSave.customerId}, #{toSave.pickUpAddress.id}, #{toSave.pickUpAddress.id}, #{toSave.pickUpDate}, #{toSave.deliveryDate}, #{toSave.cleanStatus}, #{toSave.totalOrderPrice})" +
+  @Insert("insert into clean_order(id, customerId, pickUpAddress, deliveryAddress, pickUpDate, deliveryDate, cleanStatus, totalOrderPrice, number) " +
+      " values (#{toSave.id}, #{toSave.customerId}, #{toSave.pickUpAddress.id}, #{toSave.pickUpAddress.id}, #{toSave.pickUpDate}, #{toSave.deliveryDate}, #{toSave.cleanStatus}, #{toSave.totalOrderPrice}, #{toSave.number})" +
       " on conflict (id) do nothing")
   void saveOrder(@Param("toSave") OrderRecord toSave);
 }
